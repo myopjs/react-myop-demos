e4c19248da5179a01f3467db961c63fca84efaa3
Hadar Geva <hadar111@gmail.com>
2025-05-07 22:36:40 +0700

    @myop/react-remote from npm

--- remote/src/main.tsx
+++ remote/src/main.tsx
@@ -2,13 +2,10 @@ import App from './App.tsx'
 import {expose} from "@myop/react-remote"
 
 expose(<App/>, 'react-demo-component', async () => {
-    const indexCss = (await import('./index.css?raw')).default;
-    const appCss = (await import('./App.css?raw')).default;
-
     return {
         styles: [
-            indexCss,
-            appCss
+            import('./index.css?raw'),
+            import('./App.css?raw'),
         ]
     }
 });
