0748b5641380220b44a312c3425cfdda93e2d9b3
Hadar Geva <hadar111@gmail.com>
2025-05-07 22:29:51 +0700

    init

--- remote/src/main.tsx
+++ remote/src/main.tsx
@@ -1,21 +1,14 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 import {expose} from "@myop/react-remote"
 
-
 expose(<App/>, 'react-demo-component', async () => {
-
-    debugger;
-
-    const indexcss = (await import('./index.css?raw')).default;
-    const appcss = (await import('./App.css?raw')).default;
+    const indexCss = (await import('./index.css?raw')).default;
+    const appCss = (await import('./App.css?raw')).default;
 
     return {
         styles: [
-            indexcss,
-            appcss
+            indexCss,
+            appCss
         ]
     }
 });
